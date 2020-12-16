e32ab0bafac78e279d34c6a76c6fccd89eabc685
Alexandru Ghiran <alexandru@rosmof.ro>
2020-12-16 11:29:46 +0200

    update comentarii

---
+++ modules/ziua1/src/test/java/ro/trc/ziua1/DespreGit.java
@@ -0,0 +1,23 @@
+package ro.trc.ziua1;
+
+/**
+ *
+ *
+ *
+ *
+ *
+ * ///
+ *
+ *
+ * */
+
+
+/*Model SVN*/
+/**
+ * fisier1.java v1 -> fisier1.java v2 (conflict) => merge
+ *
+ *
+ * */
+
+public class DespreGit {
+}
