2fd63792f712b3754df7a371a0525bb80554e06b
Alexandru Ghiran <alexandru@rosmof.ro>
2020-12-16 11:55:42 +0200

    update functionalitate noua

--- modules/ziua1/src/test/java/ro/trc/ziua1/DespreGit.java
+++ modules/ziua1/src/test/java/ro/trc/ziua1/DespreGit.java
@@ -1,18 +1,27 @@
 package ro.trc.ziua1;
 
 /**
- *
- *
- *
- *
- *
- * ///
- *
- *
- * */
+ * DEV => GIT LOCAL     <>   GIT CENTRAL
+ * ^
+ * DEVELOPER
+ * <p>
+ * 2) update        trage update-uri de pe central => a.java
+ * <p>
+ * 1000 fisiere -> 1 fisier
+ * <p>
+ * versiune 1.1.0
+ * absolut tot
+ * versiunea 1.1.1
+ * absolut tot
+ * <p>
+ * fisier1.java    => hash => 1234
+ * fisier2.java    => hash => 4567
+ * fisier3.java    => hash => 4567
+ */
 
 
 /*Model SVN*/
+
 /**
  * fisier1.java v1 -> fisier1.java v2 (conflict) => merge
  *
@@ -20,4 +29,13 @@ package ro.trc.ziua1;
  * */
 
 public class DespreGit {
+    public void metodaNoua() {
+
+    }
+
+    public void FunctionalitateNoua() throws Exception {
+        System.out.println("afiseaza functionalitate noua");
+    }
+
+
 }
